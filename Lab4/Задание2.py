827f3c54de66ed38f139a39722719274f3c9fb0a
annmyshkina <ann.myshkina14@gmail.com>
2024-11-15 23:57:19 +0300

    Update Задание2.py

--- Lab4/Задание2.py
+++ Lab4/Задание2.py
@@ -1,32 +1,26 @@
-# TODO импортировать необходимые молули
 import json
 import csv
 
 INPUT_FILENAME = "input.csv"
 OUTPUT_FILENAME = "output.json"
 
+
 def task() -> None:
-    ... # TODO считать содержимое csv файла
+    """
+    Считывает данные из CSV файла и записывает их в JSON файл с отступами.
+    """
+    with open(INPUT_FILENAME, "r", newline="", encoding="utf-8") as csv_file:
+        reader = csv.DictReader(csv_file)
+        data = [row for row in reader]
 
-    a = open(INPUT_FILENAME, "r")
-    b = open(OUTPUT_FILENAME, "w")
-
-    reader = csv.DictReader(a)
-    c = []
-    for i in reader:
-        c.append(i)
-
-    ... # TODO Сериализовать в файл с отступами равными 4
-
-    json.dump(c, b, indent = 4)
-    b.close()
-    a.close()
+    with open(OUTPUT_FILENAME, "w", encoding="utf-8") as json_file:
+        json.dump(data, json_file, indent=4)
 
 
-if name == '__main__':
+if __name__ == "__main__":
     # Нужно для проверки
     task()
 
-    with open(OUTPUT_FILENAME) as output_f:
-        for line in output_f:
+    with open(OUTPUT_FILENAME, encoding="utf-8") as output_file:
+        for line in output_file:
             print(line, end="")
