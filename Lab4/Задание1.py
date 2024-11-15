31df8b094d74d347878c7396143795132c4bc143
annmyshkina <ann.myshkina14@gmail.com>
2024-11-15 23:56:01 +0300

    Update Задание1.py

--- Lab4/Задание1.py
+++ Lab4/Задание1.py
@@ -1,5 +1,3 @@
-# TODO решите задачу
-
 def task() -> float:
     a = 0
     sch = 0
@@ -10,13 +8,14 @@ def task() -> float:
     a = f.readlines()
     for i in range(len(a)):
         if a[i].find("score") != -1:
-            ch1 = float(a[i][((a[i].find("score"))+8):len(a[i])-2])
-        elif (a[i].find("weight")) != -1:
-            ch2 = float(a[i][((a[i].find("weight"))+9):len(a[i])-1])
-        if(ch1 != 0) and (ch2 != 0):
-            sum_ += ch1*ch2
+            ch1 = float(a[i][(a[i].find("score") + 8): len(a[i]) - 2])
+        elif a[i].find("weight") != -1:
+            ch2 = float(a[i][(a[i].find("weight") + 9): len(a[i]) - 1])
+        if (ch1 != 0) and (ch2 != 0):
+            sum_ += ch1 * ch2
             ch1, ch2 = 0, 0
     f.close()
     return round(sum_, 3)
+
 
 print(task())
