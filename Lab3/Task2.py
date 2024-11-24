37ceea810ce5501f2de9f595a2b61ac256894205
annmyshkina <ann.myshkina14@gmail.com>
2024-11-24 14:39:48 +0300

    Update Task2.py

--- Lab3/Task2.py
+++ Lab3/Task2.py
@@ -1,7 +1,7 @@
 # TODO Напишите функцию find_common_participants
-def find_common_participants(str1, str2, str3=","):
-    first_group = set(str1.split(str3))
-    second_group = str2.split(str3)
+def find_common_participants(group_first, group_second, split_str=","):
+    first_group = set(group_first.split(split_str))
+    second_group = group_second.split(split_str)
     set_result = first_group.intersection(second_group)
     common_participants = []
     for i in set_result:
@@ -9,7 +9,8 @@ def find_common_participants(str1, str2, str3=","):
     common_participants.sort()
     return common_participants
 
+
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
-print (find_common_participants (participants_first_group, participants_second_group, "|"))
+print(find_common_participants(participants_first_group, participants_second_group, "|"))
 # TODO Провеьте работу функции с разделителем отличным от запятой
